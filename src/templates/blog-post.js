8085d7970af76a9117550cdefb97067630ecc0cc
Akshar Patel <akshar.patel.47@gmail.com>
2017-10-04 00:03:17 +0530

    feat: update picker views post

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -2,10 +2,36 @@ import React from "react";
 
 export default ({ data }) => {
   const post = data.markdownRemark;
+  const tags = (post.frontmatter.tags || []).map(t => (
+    <li
+      style={{
+        padding: "0.05rem 0.35rem",
+        marginRight: "10px",
+        background: "lightgrey",
+        borderRadius: "5px"
+      }}
+      key={t}
+    >
+      {t}
+    </li>
+  ));
   return (
     <div>
       <h2>{post.frontmatter.title}</h2>
+      <hr />
       <div dangerouslySetInnerHTML={{ __html: post.html }} />
+      <hr />
+      <ul
+        style={{
+          listStyle: "none",
+          display: "flex",
+          margin: "0",
+          padding: 0,
+          fontSize: "0.75rem"
+        }}
+      >
+        {tags}
+      </ul>
     </div>
   );
 };
@@ -16,6 +42,7 @@ export const query = graphql`
       html
       frontmatter {
         title
+        tags
       }
     }
   }
