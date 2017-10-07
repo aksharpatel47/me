9d81b0fd064175437fa71f452a61ab2b546a8f25
Akshar Patel <akshar.patel.47@gmail.com>
2017-10-07 23:04:24 +0530

    fix: render table of contents in post

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -1,7 +1,26 @@
 import React from "react";
 
+function renderTableOfContents(toc) {
+  if (
+    toc === null ||
+    toc === undefined ||
+    typeof toc !== "string" ||
+    toc.length === 0
+  ) {
+    return null;
+  }
+
+  return (
+    <div>
+      <h3>Table Of Contents</h3>
+      <div dangerouslySetInnerHTML={{ __html: toc }} />
+    </div>
+  );
+}
+
 export default ({ data }) => {
   const post = data.markdownRemark;
+  console.log(data);
   const tags = (post.frontmatter.tags || []).map(t => (
     <li
       style={{
@@ -15,9 +34,16 @@ export default ({ data }) => {
       {t}
     </li>
   ));
+  const disqusShortname = "aksharpatel47-1";
+  const disqusConfig = {
+    url: "https://aksharpatel47.com/" + post.fields.slug,
+    identifier: post.fields.slug,
+    title: post.frontmatter.titles
+  };
   return (
     <div>
       <h2>{post.frontmatter.title}</h2>
+      {renderTableOfContents(post.tableOfContents)}
       <hr />
       <div dangerouslySetInnerHTML={{ __html: post.html }} />
       <hr />
@@ -40,9 +66,13 @@ export const query = graphql`
   query BlogPostQuery($slug: String!) {
     markdownRemark(fields: { slug: { eq: $slug } }) {
       html
+      tableOfContents
       frontmatter {
         title
         tags
+      }
+      fields {
+        slug
       }
     }
   }
