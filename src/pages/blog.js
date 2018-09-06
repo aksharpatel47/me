60681b45559e37e506d5097d8ef1af0e59420f72
Akshar Patel <akshar.patel.47@gmail.com>
2018-09-05 19:58:40 -0700

    feat: scaffold blog and portfolio pages

---
+++ src/pages/blog.js
@@ -0,0 +1,3 @@
+import React from 'react'
+
+export default () => <div>This will be where the blog will go.</div>
--- src/pages/index.js
+++ src/pages/index.js
@@ -9,6 +9,10 @@ const IndexPage = () => (
     <p>Welcome to your new Gatsby site.</p>
     <p>Now go build something great.</p>
     <Link to="/page-2/">Go to page 2</Link>
+    <br />
+    <Link to="/blog">Blog</Link>
+    <br />
+    <Link to="/portfolio">Portfolio</Link>
   </Layout>
 )
 
