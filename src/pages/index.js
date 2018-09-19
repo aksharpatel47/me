29362f5e7aedf01fcf6ba6fd6ac97b0817d8e1c8
Akshar Patel <akshar.patel.47@gmail.com>
2018-09-19 00:08:25 -0700

    feat: bootstrap barebones front page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,19 +1,33 @@
 import React from 'react'
 import { Link } from 'gatsby'
 
-import Layout from '../components/layout'
-
 const IndexPage = () => (
-  <Layout>
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <Link to="/page-2/">Go to page 2</Link>
-    <br />
-    <Link to="/blog">Blog</Link>
-    <br />
-    <Link to="/portfolio">Portfolio</Link>
-  </Layout>
+  <div>
+    <title>Akshar Patel</title>
+    <h1>Akshar Patel</h1>
+    <h2>Full Stack Dev</h2>
+    <h2>AI & ML Student</h2>
+    <div>
+      <Link to="blog">Blog</Link>
+      &nbsp;
+      <Link to="portfolio">Portfolio</Link>
+    </div>
+    <div>
+      <a href="https://twitter.com/aksharpatel47" target="_blank">
+        Twitter
+      </a>
+      &nbsp;
+      <a href="https://github.com/aksharpatel47" target="_blank">
+        Github
+      </a>
+      &nbsp;
+      <a href="https://linkedin.com/in/aksharpatel47" target="_blank">
+        Linkedin
+      </a>
+      &nbsp;
+      <a href="mailto:me@aksharpatel.com">Email</a>
+    </div>
+  </div>
 )
 
 export default IndexPage
