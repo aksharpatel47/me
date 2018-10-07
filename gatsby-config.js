bba16f6a1fb772e2dc79b0e09d343459c9ebf28c
Akshar Patel <akshar.patel.47@gmail.com>
2018-10-06 18:30:42 -0700

    feat: add google analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -34,12 +34,12 @@ module.exports = {
     },
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
-    // {
-    //   resolve: `gatsby-plugin-google-analytics`,
-    //   options: {
-    //     //trackingId: `ADD YOUR TRACKING ID HERE`,
-    //   },
-    // },
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        trackingId: `UA-51408306-2`,
+      },
+    },
     // `gatsby-plugin-feed`,
     {
       resolve: `gatsby-plugin-manifest`,
