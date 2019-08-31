03e27128c9f4723c1997fae6e9841830fa33fe61
Akshar Patel <akshar.patel.47@gmail.com>
2019-08-30 23:45:38 -0700

    feat: add command to publish in the ssh connection

--- publish.sh
+++ publish.sh
@@ -1,5 +1,7 @@
+#!/bin/bash
+
 yarn build
 tar -cvzf public.tar.gz ./public
 scp -i ~/.ssh/server public.tar.gz akshar@104.225.222.82:~/me
-ssh -i ~/.ssh/server akshar@104.225.222.82
+ssh -i ~/.ssh/server akshar@104.225.222.82 "(cd me && tar -xvzf public.tar.gz)"
 rm public.tar.gz
