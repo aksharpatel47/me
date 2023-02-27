241b73fe13f5b7f18d0961e5ca761d2ccac4e970
Akshar Patel <akshar.patel.47@gmail.com>
2023-02-27 23:58:06 +0530

    add react testing lib

---
+++ src/tests/home.spec.tsx
@@ -0,0 +1,11 @@
+import { render, screen } from "@testing-library/react";
+import React from "react";
+import Home from "../app/page";
+
+describe("Home", () => {
+  it("should render the home page", () => {
+    render(<Home />);
+
+    expect(screen.getByText("Akshar Patel")).toBeInTheDocument();
+  });
+});
