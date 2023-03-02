41efca0b4f2cd122016e9150424aa55221b83f26
Akshar Patel <akshar.patel.47@gmail.com>
2023-03-02 20:01:01 +0530

    add links to social (#50)
    * add links to social
    
    * add email link
    
    * update build-pr.yml

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,6 +9,31 @@ export default function Home() {
     <main className={styles.card}>
       <h1>Akshar Patel</h1>
       <h2>Staff Software Engineer</h2>
+      <nav>
+        <a
+          className={styles.code}
+          href="https://github.com/aksharpatel47"
+          target="_blank"
+        >
+          GitHub
+        </a>
+        <br />
+        <a
+          className={styles.code}
+          href="https://www.linkedin.com/in/aksharpatel47"
+          target="_blank"
+        >
+          LinkedIn
+        </a>
+        <br />
+        <a
+          className={styles.code}
+          href="mailto:akshar.patel.47@gmail.com"
+          target="_blank"
+        >
+          Email
+        </a>
+      </nav>
     </main>
   );
 }
