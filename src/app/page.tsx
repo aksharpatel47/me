92423b7005f3b137883052492ec6b5eec2a38013
Akshar Patel <akshar.patel.47@gmail.com>
2023-03-02 19:12:38 +0530

    small change

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,14 @@
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
-import styles from './page.module.css'
+import Image from "next/image";
+import { Inter } from "next/font/google";
+import styles from "./page.module.css";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
-    <main className={styles.main}>
+    <main className={styles.card}>
       <h1>Akshar Patel</h1>
       <h2>Staff Software Engineer</h2>
     </main>
-  )
+  );
 }
